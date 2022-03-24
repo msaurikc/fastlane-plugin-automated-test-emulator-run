860d05e849db9d13a23fb1e49a75c6890ca64578
msaurikc <miguel.sauri@kinandcarta.com>
2022-03-24 16:42:58 -0300

    Fix formatting

--- lib/fastlane/plugin/automated_test_emulator_run/factory/adb_controller_factory.rb
+++ lib/fastlane/plugin/automated_test_emulator_run/factory/adb_controller_factory.rb
@@ -11,52 +11,53 @@ module Fastlane
     end
 
     class AdbControllerFactory
-  
-        def self.get_adb_controller(params)
-          UI.message(["Preparing commands for Android ADB"].join(" ").yellow)
 
-          # Get paths
-          path_sdk = "#{params[:SDK_path]}"
-          path_avdmanager_binary = path_sdk + "/tools/bin/avdmanager"
-          path_adb = path_sdk + "/platform-tools/adb"
+      def self.get_adb_controller(params)
+        UI.message(["Preparing commands for Android ADB"].join(" ").yellow)
 
-          # ADB shell command parts
-          sh_stop_adb = "kill-server"
-          sh_start_adb = "start-server"
-          sh_devices_adb = "devices"
-          sh_wait_for_device_adb = "wait-for-device"
-          sh_list_avd_adb = "list avd"
+        # Get paths
+        path_sdk = "#{params[:SDK_path]}"
+        path_avdmanager_binary = path_sdk + "/tools/bin/avdmanager"
+        path_adb = path_sdk + "/platform-tools/adb"
 
-          # Assemble ADB controller
-          adb_controller = ADB_Controller.new
-          adb_controller.command_stop = [
-           path_adb,
-           sh_stop_adb
-           ].join(" ")
-  
-          adb_controller.command_start = [
-           path_adb,
-           sh_start_adb
-           ].join(" ")
-  
-          adb_controller.command_get_devices = [
-           path_adb,
-           sh_devices_adb
-           ].join(" ")
-  
-          adb_controller.command_wait_for_device = [
-           path_adb,
-           sh_wait_for_device_adb
-           ].join(" ")
+        # ADB shell command parts
+        sh_stop_adb = "kill-server"
+        sh_start_adb = "start-server"
+        sh_devices_adb = "devices"
+        sh_wait_for_device_adb = "wait-for-device"
+        sh_list_avd_adb = "list avd"
 
-          adb_controller.adb_path = path_adb
+        # Assemble ADB controller
+        adb_controller = ADB_Controller.new
+        adb_controller.command_stop = [
+          path_adb,
+          sh_stop_adb
+        ].join(" ")
 
-          adb_controller.command_get_avds = [
-           path_avdmanager_binary, 
-           sh_list_avd_adb].join(" ").chomp
-  
-          return adb_controller
-        end 
+        adb_controller.command_start = [
+          path_adb,
+          sh_start_adb
+        ].join(" ")
+
+        adb_controller.command_get_devices = [
+          path_adb,
+          sh_devices_adb
+        ].join(" ")
+
+        adb_controller.command_wait_for_device = [
+          path_adb,
+          sh_wait_for_device_adb
+        ].join(" ")
+
+        adb_controller.adb_path = path_adb
+
+        adb_controller.command_get_avds = [
+          path_avdmanager_binary,
+          sh_list_avd_adb
+        ].join(" ").chomp
+
+        return adb_controller
+      end
     end
   end
 end
